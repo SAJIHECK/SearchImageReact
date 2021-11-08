1a7a2998a6c8b75885ebd9fe98aea2192aa1f464
sajeeh <sajeehck@gmail.com>
2021-11-08 17:38:32 +0530

    second comment

---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class SearchBar extends React.Component{
+render(){
+    return (<div>
+        <form>
+            <input/>
+        </form>
+
+    </div>);
+}
+}
+export default SearchBar;
