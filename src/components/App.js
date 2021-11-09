94372dbc5bc6f18b92148887525b24d85ddac0f0
sajeeh <sajeehck@gmail.com>
2021-11-09 17:27:56 +0530

    my 09 nov comment

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
+import { render } from '@testing-library/react';
 import React from 'react';
 import SearchBar from './SearchBar';
 
 
-const App = () => { 
-
-return <div className="ui container" style={{marginTop:'20px'}}><SearchBar/></div>;
-
-};
+class App extends React.Component {
+onSearchSubmit(term){
+    console.log(term);
+}
+    
+render(){
+    return(
+          <div className="ui container" 
+             style={{marginTop:'20px'}}>
+                 <SearchBar onSubmit={this.onSearchSubmit} />
+          </div>
+         );
+    }
+}
 
 export default App;
